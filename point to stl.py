ea1c93a7851d82aad8e3d970c9b220b18717ccf6
Levi-Murawa <60005634+Levi-Murawa@users.noreply.github.com>
2021-10-23 23:35:52 +0200

    Update point to stl.py

--- point to stl.py
+++ point to stl.py
@@ -1,9 +1,32 @@
+import open3d as o3d
 from stl import mesh
-num_triangles=len(fin_list)
-data = np.zeros(num_triangles, dtype=mesh.Mesh.dtype)
-for i in range(num_triangles):
-    #I did not know how to use numpy-arrays in this case. This was the major roadblock
-    # assign vertex co-ordinates to variables to write into mesh
-    data["vectors"][i] = np.array([[v1x, v1y, v1z],[v2x, v2y, v2z],[v3x, v3y, v3z]])
-m=mesh.Mesh(data)
-m.save('filename.stl')
+
+
+input_path="C:/Users/Tomek/PycharmProjects/objetosc/" ##Droga do pliku
+output_path="C:/Users/Tomek/PycharmProjects/objetosc/render/" #Gdzie zapisywac (Nie wykorzystane w tej wersji)
+dataname=("sphere_spiral_700.xyz")
+
+
+pcd = o3d.io.read_point_cloud(dataname) ##zaczytanie pliku
+downpcd = pcd.voxel_down_sample(voxel_size=0.05) #zmniejszanie rozmiaru, mniejsza liczba = wieksza precyzja)
+
+downpcd.estimate_normals(
+    search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.3, max_nn=300)) #Kalkulacja normalnych
+
+downpcd.orient_normals_consistent_tangent_plane(10) #Poprawka noramlanych
+
+#o3d.visualization.draw_geometries([downpcd], point_show_normal=True) #Wizualizacja normalnych
+
+poisson_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(downpcd, depth=8, width=0, scale=1.1, linear_fit=False)[0]
+#Jedna z opcji znajdowania i rekonstrukcji ścian. Wymaga normalnych wyliczonych wcześniej
+
+#o3d.visualization.draw_geometries([poisson_mesh]) #prezentacja pełnej bryły
+
+poisson_mesh.compute_vertex_normals() #Przygotowanie do zapisu
+print(poisson_mesh)
+o3d.io.write_triangle_mesh("tymczasowy.stl", poisson_mesh) #Zapis pliku w formacie STL
+
+your_mesh = mesh.Mesh.from_file("tymczasowy.stl") #Zaczytanie danych STL
+
+volume, cog, inertia = your_mesh.get_mass_properties()
+print("Objetosc                                  = {0}".format(volume)) #wyswietlenie wyniku
